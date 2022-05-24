5e9d0c09ef4e19e0c59b10c143b8457cd021f878
Ibuki-Matsumoto <n4rvs3apex@gmail.com>
2022-05-24 09:36:15 +0900

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const config = {
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+  images: {
+    domains: ['apexlegendsstatus.com'],
+  },
+};
+
+module.exports = withPWA(config);
